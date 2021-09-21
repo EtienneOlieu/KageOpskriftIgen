0fe1d0e9a5589bdf45299376921a6c82d48c92e4
Horrid Entity <etienne.olieu@gmail.com>
2021-09-21 13:28:53 +0200

    Second commit. Working with values

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        Cake brownie = new Cake(4);
+
+        System.out.println(brownie);
+
+        System.out.println(brownie.totalVægtIngredienser());
+
+        System.out.println(brownie.totalVægtFærdigKage());
+
+        System.out.println(brownie.samletEnergiIndhold());
+
+    }
+
+}
